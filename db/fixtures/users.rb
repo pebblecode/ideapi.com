a93ac636fff07b1eb46ec397ebbd5ee864eb0aec
Jason Cale <jasoncale@jason-cales-macbook-pro-2.local>
2009-06-09 11:08:13 +0100

    standarising seed data

--- db/fixtures/users.rb
+++ db/fixtures/users.rb
@@ -1,26 +1,26 @@
 Author.seed(:login, :email) do |s|  
-  s.login = "jason"   
-  s.email = "jase@jaseandtonic.com"
-  s.password = "testing"
-  s.password_confirmation = "testing"   
+  s.login = "client"   
+  s.email = "client@ideapi.net"
+  s.password = "password"
+  s.password_confirmation = "password"   
 end
 
 Creative.seed(:login, :email) do |s|
   s.login = "creative"   
-  s.email = "creative@jaseandtonic.com"
-  s.password = "testing"
-  s.password_confirmation = "testing"
+  s.email = "creative@ideapi.net"
+  s.password = "password"
+  s.password_confirmation = "password"
 end
 
-jason = Author.find_by_login("jason")
+client = Author.find_by_login("client")
 template = BriefTemplate.find_by_title("Default")
 creative = Creative.find_by_login("creative")
 
-if jason && template && creative
-  draft_brief = Brief.create(:title => "Draft brief", :author => jason, :brief_template => template)
+if client && template && creative
+  draft_brief = Brief.create(:title => "Draft brief", :author => client, :brief_template => template)
   
   puts "\t- creating published brief"
-  to_publish = Brief.create(:title => "Published brief", :author => jason, :brief_template => template)
+  to_publish = Brief.create(:title => "Published brief", :author => client, :brief_template => template)
   
   puts "\t- stubbling out the answers"
   print "\t"
@@ -32,11 +32,11 @@ if jason && template && creative
   puts "\n\t- publishing brief"
   to_publish.publish!
   
-  to_publish.brief_answers.first.creative_questions.create(
-    :creative => creative, 
-    :body => "Why do we scream at each other?", 
-    :answer => "This is what it sounds like when do doves cry"
-  )
-  
+  # to_publish.brief_answers.first.creative_questions.create(
+  #    :creative => creative, 
+  #    :body => "Why do we scream at each other?", 
+  #    :answer => "This is what it sounds like when do doves cry"
+  #  )
+  #  
 end
 
