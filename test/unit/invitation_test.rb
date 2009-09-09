c34f0e9cc5dc27ea33a61ddd5d93b5f8ce0c05a6
Jason Cale <jasoncale@jason-cales-macbook-pro-2.local>
2009-09-09 16:38:08 +0100

    added existing_user? to invitation

--- test/unit/invitation_test.rb
+++ test/unit/invitation_test.rb
@@ -1,8 +1,23 @@
 require 'test_helper'
 
 class InvitationTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  context "should know if it references an existing user" do
+    setup do
+      @user = User.make
+      @invited = User.make
+      @brief = Brief.make(:published, { :user => @user })
+      
+      @invite = @user.invitations.create(
+        {:recipient_email => @invited.email, :redeemable => @brief}
+      )
+    end
+
+    should "respond to existing_user?" do
+      assert @invite.existing_user?
+    end
+
   end
+  
+  
 end
