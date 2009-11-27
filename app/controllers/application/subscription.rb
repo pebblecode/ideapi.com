81c90db26281a0494e970c300a26d267039e55cb
Jason Cale <jasoncale@jason-cales-macbook-pro-2.local>
2009-11-27 15:56:50 +0000

    account expiration

--- app/controllers/application/subscription.rb
+++ app/controllers/application/subscription.rb
@@ -40,7 +40,13 @@ class ApplicationController < ActionController::Base
   end
   
   def check_for_expired_account
-    redirect_to account_path unless current_account.subscription.current?
+    unless current_account.subscription.current?  
+      if current_account.admin?(current_user)
+        redirect_to account_path
+      else
+        redirect_to '/account_expired' and return
+      end
+    end
   end
   
 end
