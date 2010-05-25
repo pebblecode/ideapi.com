85d275cab1cfd3347c74ff1021b78abb4a7697bd
Alex Butcher <alexbutcher@AFBmacbookpro-2.local>
2010-05-25 15:06:23 +0100

    Changed support email address to support@ideapi.net

--- app/models/user_feedback.rb
+++ app/models/user_feedback.rb
@@ -3,7 +3,8 @@ class UserFeedback < ActionMailer::Base
   def feedback(feedback, sent_at = Time.now)
     @subject = 'Feedback from user'
     @body[:feedback] = feedback
-    @recipients = 'ticket+abutcher.32755-u8cs2ma4@lighthouseapp.com'
+    #@recipients = 'ticket+abutcher.32755-u8cs2ma4@lighthouseapp.com'
+    @recipients = 'support@ideapi.net'
     @from = 'alex@abutcher.co.uk'
     @sent_on = sent_at
   end
