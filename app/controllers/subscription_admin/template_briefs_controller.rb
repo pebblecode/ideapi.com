3699016ba5f2137faf4916e3ab9dc5031baffd6d
Vincent Martínez <vincent@pebbleit.com>
2010-06-07 10:49:29 +0100

    Adding sort method to controller.

--- app/controllers/subscription_admin/template_briefs_controller.rb
+++ app/controllers/subscription_admin/template_briefs_controller.rb
@@ -12,6 +12,15 @@ class SubscriptionAdmin::TemplateBriefsController < ApplicationController
   def url_helper_prefix
     "admin_"
   end
+  
+  def sort
+    @template = TemplateBriefs.find(params[:id])
+    @template.template_questions.each do |question|
+      question.position = params['template_questions'].index(question.id.to_s) + 1
+      question.save
+    end
+    render :nothing => true
+  end
 
   make_resourceful do
     actions :all
@@ -23,7 +32,7 @@ class SubscriptionAdmin::TemplateBriefsController < ApplicationController
     before :edit do
       current_object.template_brief_questions.build  
     end
-      
+    
   end
 
   private
