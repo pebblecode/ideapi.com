ad9c8d94d112c08265c5d5902b256b804b061b70
Vincent Martínez <vincent@pebbleit.com>
2010-08-20 15:39:17 +0100

    schema conflict

--- db/schema.rb
+++ db/schema.rb
@@ -9,8 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100818135945) do
-
+ActiveRecord::Schema.define(:version => 20100818115953) do
   create_table "account_template_briefs", :force => true do |t|
     t.integer "account_id"
     t.integer "template_brief_id"
