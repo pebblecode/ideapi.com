9c64190398dc91f32f57c145a9cfb774c6af36e7
Vincent Martínez <vincent@pebbleit.com>
2010-08-25 09:14:10 +0100

    Setup headers not working as expected. Changed to per method settings

--- app/models/notification_mailer.rb
+++ app/models/notification_mailer.rb
@@ -16,7 +16,11 @@ class NotificationMailer < ActionMailer::Base
   # Fixed in rails 3.x
   
   def user_added_to_brief(user_brief, sent_at = Time.now)
-    setup_headers
+    from      "no-reply@ideapi.com"
+    headers   "return-path" => 'no-reply@ideapi.com'
+    reply_to  "no-reply@ideapi.com"
+    content_type "text/html"
+
     recipients  user_brief.user.email
     subject     build_subject(user_brief.brief.account.name, "You have been invited to collaborate", user_brief.brief.title)
     body        :user_brief => user_brief
@@ -24,7 +28,11 @@ class NotificationMailer < ActionMailer::Base
   end
   
   def user_idea_reviewed_on_brief(proposal, sent_at = Time.now)
-    setup_headers
+    from      "no-reply@ideapi.com"
+    headers   "return-path" => 'no-reply@ideapi.com'
+    reply_to  "no-reply@ideapi.com"
+    content_type "text/html"
+
     recipients  proposal.user.email
     subject     build_subject(proposal.brief.account.name, "Your idea has been reviewed", proposal.brief.title)
     body        :proposal => proposal, :brief => proposal.brief
@@ -32,7 +40,11 @@ class NotificationMailer < ActionMailer::Base
   end
   
   def user_question_answered_on_brief(question, sent_at = Time.now)
-    setup_headers
+    from      "no-reply@ideapi.com"
+    headers   "return-path" => 'no-reply@ideapi.com'
+    reply_to  "no-reply@ideapi.com"
+    content_type "text/html"
+
     recipients  question.user.email
     subject     build_subject(question.brief.account.name, "Your question has been answered", question.brief.title)
     body        :question => question
@@ -41,7 +53,11 @@ class NotificationMailer < ActionMailer::Base
 
   
   def user_role_changed_on_brief(user_brief, sent_at = Time.now)
-    setup_headers
+    from      "no-reply@ideapi.com"
+    headers   "return-path" => 'no-reply@ideapi.com'
+    reply_to  "no-reply@ideapi.com"
+    content_type "text/html"
+
     recipients  user_brief.user.email
     reply_to    email_address(user_brief.brief.account.name)
     subject     build_subject(user_brief.brief.account.name, "Your role is now #{user_brief.role}", user_brief.brief.title)
@@ -50,7 +66,11 @@ class NotificationMailer < ActionMailer::Base
   end
   
   def user_invited_to_account(user, account, sent_at = Time.now)
-    setup_headers
+    from      "no-reply@ideapi.com"
+    headers   "return-path" => 'no-reply@ideapi.com'
+    reply_to  "no-reply@ideapi.com"
+    content_type "text/html"
+
     recipients  user.email
     reply_to    email_address(account.name)
     subject     build_subject(account.name, "You now have an ideapi.com account", "ideapi")
@@ -60,7 +80,11 @@ class NotificationMailer < ActionMailer::Base
   
   
   def user_made_approver_on_brief(brief, sent_at = Time.now)
-    setup_headers
+    from      "no-reply@ideapi.com"
+    headers   "return-path" => 'no-reply@ideapi.com'
+    reply_to  "no-reply@ideapi.com"
+    content_type "text/html"
+
     recipients  brief.approver.email
     reply_to    email_address(brief.account.name)
     subject     build_subject(brief.account.name, "You've been made an approver", brief.title)
@@ -70,7 +94,11 @@ class NotificationMailer < ActionMailer::Base
   end
   
   def password_reset_instructions(user, account)
-    setup_headers
+    from      "no-reply@ideapi.com"
+    headers   "return-path" => 'no-reply@ideapi.com'
+    reply_to  "no-reply@ideapi.com"
+    content_type "text/html"
+
     subject       "Password Reset Instructions"  
     recipients    user.email
     sent_on       Time.now
@@ -78,7 +106,11 @@ class NotificationMailer < ActionMailer::Base
   end
   
   def to_approver_idea_submitted_on_brief(approver, proposal, sent_at = Time.now)
-    setup_headers
+    from      "no-reply@ideapi.com"
+    headers   "return-path" => 'no-reply@ideapi.com'
+    reply_to  "no-reply@ideapi.com"
+    content_type "text/html"
+
     recipients  approver.email
     reply_to    email_address(proposal.brief.account.name)
     subject     build_subject(proposal.brief.account.name, "An idea has been submitted for review", proposal.brief.title)
@@ -87,7 +119,11 @@ class NotificationMailer < ActionMailer::Base
   end
   
   def new_question_on_brief(question, sent_at = Time.now)
-    setup_headers
+    from      "no-reply@ideapi.com"
+    headers   "return-path" => 'no-reply@ideapi.com'
+    reply_to  "no-reply@ideapi.com"
+    content_type "text/html"
+
     recipients  question.brief.authors.collect{ |user| user.email }.compact
     reply_to    email_address(question.brief.account.name)
     subject     build_subject(question.brief.account.name, "A question has been posted", question.brief.title)
@@ -96,7 +132,11 @@ class NotificationMailer < ActionMailer::Base
   end
   
   def new_comment_on_brief(comment, sent_at = Time.now)
-    setup_headers
+    from      "no-reply@ideapi.com"
+    headers   "return-path" => 'no-reply@ideapi.com'
+    reply_to  "no-reply@ideapi.com"
+    content_type "text/html"
+
     recipients  comment.commentable.users.collect{ |user| user.email }.compact - [comment.user.email]
     subject     build_subject(comment.commentable.account.name, "A comment has been posted", comment.commentable.title)
     body        :comment => comment
@@ -104,7 +144,11 @@ class NotificationMailer < ActionMailer::Base
   end
   
   def new_comment_on_idea(comment, sent_at = Time.now)
-    setup_headers
+    from      "no-reply@ideapi.com"
+    headers   "return-path" => 'no-reply@ideapi.com'
+    reply_to  "no-reply@ideapi.com"
+    content_type "text/html"
+
     # Authors and Approver
     recipients  comment.commentable.brief.authors.collect{ |author| author.email }.push(comment.commentable.brief.approver.email).compact.uniq - [comment.user.email]
     subject     build_subject(comment.commentable.brief.account.name, "A comment has been posted", comment.commentable.brief.title)
@@ -113,7 +157,11 @@ class NotificationMailer < ActionMailer::Base
   end
   
   def brief_section_updated(brief, user, items, sent_at = Time.now)
-    setup_headers
+    from      "no-reply@ideapi.com"
+    headers   "return-path" => 'no-reply@ideapi.com'
+    reply_to  "no-reply@ideapi.com"
+    content_type "text/html"
+    
     recipients  brief.users.collect{ |user| user.email }.compact
     subject     build_subject(brief.account.name, "Brief updated", brief.title)
     body        :brief => brief, :user => user, :items => items
@@ -121,7 +169,11 @@ class NotificationMailer < ActionMailer::Base
   end
   
   def brief_updated(brief, sent_at = Time.now)
-    setup_headers
+    from      "no-reply@ideapi.com"
+    headers   "return-path" => 'no-reply@ideapi.com'
+    reply_to  "no-reply@ideapi.com"
+    content_type "text/html"
+    
     recipients  brief.users.collect{ |user| user.email }.compact
     subject     build_subject(brief.account.name, "Brief updated", brief.title)
     body        :brief => brief
