459e37c2c7278496ea90adc22142a66dae65500b
George Ornbo <george@shapeshed.com>
2010-09-03 14:38:08 +0100

    scoping tag suggestions to current account

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,7 +3,7 @@ class TagsController < ApplicationController
   before_filter :require_user
 
   def index
-    @tags = current_account.briefs.tag_counts_on(:tags)
+    @tags = current_account.owned_tags
     respond_to do |format|
       format.json {render :layout => false}
     end
