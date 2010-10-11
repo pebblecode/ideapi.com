d79d5055f28fc3d7ab5480eacd9a0da3e9acec28
Tak Tran <ttt@pebbleit.com>
2010-10-11 11:47:47 +0100

    Cleaned up old login code + adding infield labels.

--- public/javascripts/marketing.js
+++ public/javascripts/marketing.js
@@ -10,39 +10,6 @@ $(document).ready(function(){
     return false;
   });
   
-  email = $('#user_session_email');
-  password = $('#user_session_password');
-  bind_account_form_switch(email);
-  bind_account_form_switch(password);
+  $(".infield-labels label").inFieldLabels();
   
-  $('#account-button.logged-out').click(function(){
-    $(this).toggleClass('active');
-    $('#account-form').slideToggle('fast');
-    
-    return false;
-  });
-
-  $('#account-button.logged-in').click(function(){
-     $(this).toggleClass('active');
-     $('#account-links').slideToggle('fast');
-
-     return false;
-   });
 });
-
-function bind_account_form_switch(object){
-  
-  if(object.val() == '' ){
-    object.addClass('empty');
-  }
-  
-  object.focus(function(){
-    object.removeClass('empty');
-  });
-  
-  object.blur(function(){
-    if( object.attr('value') == "" ){
-      object.addClass('empty');
-    }
-  });
-}
