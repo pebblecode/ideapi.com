7596a7e251e633193889ff27269353570ab73495
Vincent Martínez <vincent@pebbleit.com>
2010-10-18 17:33:39 +0100

    Adding craken config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,3 +4,12 @@ set(:deploy_to) { "/var/www/vhosts/ideapi.com/httpdocs" }
 set :repository,  "git@apu.pebbleit.com:ideapi.com.git"
 set :branch, "master"
 set :deploy_via, :remote_cache
+
+
+namespace :craken do
+  desc "Install craken"
+  task :install, :roles => :cron do
+    set :raketab_rails_env, "staging"
+    run "cd #{current_path} && rake app_name=#{application} deploy_path=#{current_path} RAILS_ENV=#{stage} craken:install"
+  end
+end
