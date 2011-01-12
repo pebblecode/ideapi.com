ec9a64a3ee0039314787cccdf5631768edd51c2c
Vincent Martínez <eykosioux@gmail.com>
2011-01-12 17:09:11 +0000

    Tidying up /pulic

--- public/javascripts/marketing.js
+++ public/javascripts/marketing.js
@@ -9,8 +9,40 @@ $(document).ready(function(){
   	nextId: "slider-next",
   	prevId: "slider-prev"
   });
-  
+  // jQuery SmoothScroll | Version 10-04-30
+  $('a[href*=#]').click(function() {
 
+     // duration in ms
+     var duration=1000;
+
+     // easing values: swing | linear
+     var easing='swing';
+
+     // get / set parameters
+     var newHash=this.hash;
+     var target=$(this.hash).offset().top;
+     var oldLocation=window.location.href.replace(window.location.hash, '');
+     var newLocation=this;
+
+     // make sure it's the same location      
+     if(oldLocation+newHash==newLocation)
+     {
+        // set selector
+        var animationSelector = null;
+        if($.browser.safari) animationSelector='body:not(:animated)';
+        else animationSelector='html:not(:animated)';
+
+        // animate to target and set the hash to the window.location after the animation
+        $(animationSelector).animate({ scrollTop: target }, duration, easing, function() {
+
+           // add new hash to the browser location
+           window.location.href=newLocation;
+        });
+
+        // cancel default click action
+        return false;
+     }
+  });
 });
 
 // For debugging
