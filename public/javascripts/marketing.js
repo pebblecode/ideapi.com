41e26111c8d6ea0a1edf0fc7b3f03c4909b2ffcb
Tak Tran <ttt@pebbleit.com>
2011-04-13 21:10:19 +0100

    Adding active li js.

--- public/javascripts/marketing.js
+++ public/javascripts/marketing.js
@@ -3,49 +3,56 @@ $(document).ready(function(){
   // For debugging: Add grid on logo click
   // turn_on_logo_grid_toggle();
   
-  $("#slider").easySlider({
-  	auto: false,
-  	continuous: true,
-  	nextId: "slider-next",
-  	prevId: "slider-prev"
+  // $("#slider").easySlider({
+  //  auto: false,
+  //  continuous: true,
+  //  nextId: "slider-next",
+  //  prevId: "slider-prev"
+  // });
+  // // jQuery SmoothScroll | Version 10-04-30
+  // $('a[href*=#]').click(function(e) {
+  // 
+  //    // duration in ms
+  //    var duration=1000;
+  // 
+  //    // easing values: swing | linear
+  //    var easing='swing';
+  // 
+  //    // get / set parameters
+  //    var newHash=this.hash;
+  //    var target=$(this.hash).offset().top;
+  //    var oldLocation=window.location.href.replace(window.location.hash, '');
+  //    var newLocation=this;
+  // 
+  //    // make sure it's the same location      
+  //    if(oldLocation+newHash==newLocation)
+  //    {
+  //       // set selector
+  //       var animationSelector = null;
+  //       if($.browser.safari) animationSelector='body:not(:animated)';
+  //       else animationSelector='html:not(:animated)';
+  // 
+  //       // animate to target and set the hash to the window.location after the animation
+  //       $(animationSelector).animate({ scrollTop: target }, duration, easing, function() {
+  // 
+  //          // add new hash to the browser location
+  //          window.location.href=newLocation;
+  //       });
+  // 
+  //       // cancel default click action
+  //       e.preventDefault();
+  //       return false;
+  //    }
+  // });
+  // $("a.overlay-video-trigger").overlay();
+
+  // Features page - set feature title li to be active if clicked, removing all other active li's
+  $('.features-index > li a.feature-title').click(function(e) {
+    $('.features-index > li').removeClass("active");
+    $(this).parent().addClass("active");
+
+    return false;
   });
-  // jQuery SmoothScroll | Version 10-04-30
-  $('a[href*=#]').click(function(e) {
-
-     // duration in ms
-     var duration=1000;
-
-     // easing values: swing | linear
-     var easing='swing';
-
-     // get / set parameters
-     var newHash=this.hash;
-     var target=$(this.hash).offset().top;
-     var oldLocation=window.location.href.replace(window.location.hash, '');
-     var newLocation=this;
-
-     // make sure it's the same location      
-     if(oldLocation+newHash==newLocation)
-     {
-        // set selector
-        var animationSelector = null;
-        if($.browser.safari) animationSelector='body:not(:animated)';
-        else animationSelector='html:not(:animated)';
-
-        // animate to target and set the hash to the window.location after the animation
-        $(animationSelector).animate({ scrollTop: target }, duration, easing, function() {
-
-           // add new hash to the browser location
-           window.location.href=newLocation;
-        });
-
-        // cancel default click action
-        e.preventDefault();
-        return false;
-     }
-  });
-  $("a.overlay-video-trigger").overlay();
-  
   
 });
 
