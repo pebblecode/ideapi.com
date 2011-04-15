276955b2b3b123b7d6b72b0eb74c47abebb34908
Tak Tran <ttt@pebbleit.com>
2011-04-15 17:54:52 +0100

    Adding sort for last login (removing last login of account owners - not in table).

--- app/controllers/subscription_admin/account_owners_controller.rb
+++ app/controllers/subscription_admin/account_owners_controller.rb
@@ -8,6 +8,15 @@ class SubscriptionAdmin::AccountOwnersController < ApplicationController
   before_filter :reject_unauthorized_hosts
   
   def index
-    @account_owners = ViewAccountOwner.all
+    sort_desc = (params[:desc] == "true" ) ? " DESC" : ""
+    @sort_by_description = ""
+    if (params[:sortby] == "last_login_at")
+      @account_owners = ViewAccountOwner.find(:all)
+      @users = User.find(:all, :order => "last_login_at #{sort_desc}")
+    else # Otherwise don't sort
+      @account_owners = ViewAccountOwner.find(:all)
+      @users = User.find(:all)        
+    end
+    
   end
 end
