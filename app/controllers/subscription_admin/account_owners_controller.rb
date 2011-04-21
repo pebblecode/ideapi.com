4bef63965c09668ee1cf11efa887f075441a61e1
Tak Tran <ttt@pebbleit.com>
2011-04-21 09:51:43 +0100

    Removing sort from controller. Handled by javascript.

--- app/controllers/subscription_admin/account_owners_controller.rb
+++ app/controllers/subscription_admin/account_owners_controller.rb
@@ -8,15 +8,7 @@ class SubscriptionAdmin::AccountOwnersController < ApplicationController
   before_filter :reject_unauthorized_hosts
   
   def index
-    sort_desc = (params[:desc] == "true" ) ? " DESC" : ""
-    @sort_by_description = ""
-    if (params[:sortby] == "last_login_at")
-      @account_owners = ViewAccountOwner.find(:all)
-      @users = User.find(:all, :order => "last_login_at #{sort_desc}")
-    else # Otherwise don't sort
-      @account_owners = ViewAccountOwner.find(:all)
-      @users = User.find(:all)        
-    end
-    
+    @account_owners = ViewAccountOwner.find(:all)
+    @users = User.find(:all)
   end
 end
