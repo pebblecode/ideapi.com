204b7797c80eef7e1554efd42f9439f0d09ed3d6
Tak Tran <ttt@pebbleit.com>
2011-05-06 18:10:34 +0100

    Removing unnecessary redirect.

--- test/integration/approvals_test.rb
+++ test/integration/approvals_test.rb
@@ -20,10 +20,8 @@ class ApprovalsTest < ActionController::IntegrationTest
 
     context "document author" do
       setup do
-        login_to_account_as(@account, @author)
+        login_to_account_as(@account, @author)        
         visit document_proposal_path(@document, @proposal)
-        assert_true(redirect?)        
-        follow_redirect!
       end
 
       should_respond_with :success
