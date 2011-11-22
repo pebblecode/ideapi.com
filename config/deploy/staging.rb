b5c115cc01851109b10a823a5ca0fd81b667aa45
Vince Martinez <eykosioux@gmail.com>
2011-11-22 15:47:33 +0000

    Updated staging recipe to use github

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,6 @@
 server "apu.pebbleit.com", :app, :web, :db, :primary => true
 set :user, "ideapi"
 set(:deploy_to) { "/var/www/vhosts/ideapi.com/httpdocs" }
-set :repository,  "git@apu.pebbleit.com:ideapi.com.git"
 set :branch, "master"
 set :deploy_via, :remote_cache
 role :cron, "apu.pebbleit.com"
